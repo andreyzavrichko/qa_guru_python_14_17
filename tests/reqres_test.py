d64a17ce6bec7438ab80e8f962395470fc655714
Andrey <andrey@zavrichko.dev>
2024-08-10 19:03:14 +0300

    hw17

--- tests/reqres_test.py
+++ tests/reqres_test.py
@@ -1,4 +1,5 @@
 import json
+from resource import path
 
 import requests
 from jsonschema.validators import validate
@@ -9,6 +10,12 @@ def test_list_users(base_url):
     assert response.status_code == 200
     assert response.json()['page'] == 2
     assert response.json()['data'] != []
+
+    schema = path('list_user.json')
+
+    with open(schema) as file:
+        f = file.read()
+        validate(response.json(), schema=json.loads(f))
 
 
 def test_single_user(base_url):
@@ -22,11 +29,17 @@ def test_single_user(base_url):
     assert response.json()['data']['email'] == email
     assert response.json()['data']['first_name'] == first_name
     assert response.json()['data']['last_name'] == last_name
+    schema = path('single_user.json')
+
+    with open(schema) as file:
+        f = file.read()
+        validate(response.json(), schema=json.loads(f))
 
 
 def test_single_user_not_found(base_url):
     response = requests.get(base_url + '/api/users/23')
     assert response.status_code == 404
+    assert response.json() == {}
 
 
 def test_list_resource(base_url):
@@ -36,6 +49,12 @@ def test_list_resource(base_url):
     assert response.json()['data'] != []
     assert response.json()['total'] == 12
     assert response.json()['support']['text'] == text
+
+    schema = path('list_resources.json')
+
+    with open(schema) as file:
+        f = file.read()
+        validate(response.json(), schema=json.loads(f))
 
 
 def test_single_resource(base_url):
@@ -49,6 +68,12 @@ def test_single_resource(base_url):
     response = requests.get(base_url + '/api/unknown/2')
     assert response.status_code == 200
     assert response.json()['data'] == data
+
+    schema = path('single_resources.json')
+
+    with open(schema) as file:
+        f = file.read()
+        validate(response.json(), schema=json.loads(f))
 
 
 def test_single_resource_not_found(base_url):
@@ -65,6 +90,12 @@ def test_login_successful(base_url):
     assert response.status_code == 200
     assert 'token' in response.json()
     assert response.json()['token']
+
+    schema = path('login_successful.json')
+
+    with open(schema) as file:
+        f = file.read()
+        validate(response.json(), schema=json.loads(f))
 
 
 def test_login_unsuccessful(base_url):
@@ -155,48 +186,3 @@ def test_patch(base_url):
     update = requests.patch(base_url + '/api/users/' + id, data=payload_update)
     assert update.status_code == 200
     assert update.json()['job'] == new_job
-
-
-def test_list_users_schema(base_url):
-    response = requests.get(base_url + '/api/users', params='page=2')
-    assert response.status_code == 200
-    # Валидация ответа от сервера
-    with open('../schemas/list_user.json') as file:
-        schema = json.load(file)
-    validate(instance=response.json(), schema=schema)
-
-
-def test_single_user_schema(base_url):
-    response = requests.get(base_url + '/api/users/2')
-    assert response.status_code == 200
-    with open('../schemas/single_user.json') as file:
-        schema = json.load(file)
-    validate(instance=response.json(), schema=schema)
-
-
-def test_list_resource_schema(base_url):
-    response = requests.get(base_url + '/api/unknown')
-    assert response.status_code == 200
-    with open('../schemas/list_resources.json') as file:
-        schema = json.load(file)
-    validate(instance=response.json(), schema=schema)
-
-
-def test_single_resource_schema(base_url):
-    response = requests.get(base_url + '/api/unknown/2')
-    assert response.status_code == 200
-    with open('../schemas/single_resources.json') as file:
-        schema = json.load(file)
-    validate(instance=response.json(), schema=schema)
-
-
-def test_login_successful_schema(base_url):
-    payload = {
-        "email": "eve.holt@reqres.in",
-        "password": "cityslicka"
-    }
-    response = requests.post(base_url + '/api/login', data=payload)
-    assert response.status_code == 200
-    with open('../schemas/login_successful.json') as file:
-        schema = json.load(file)
-    validate(instance=response.json(), schema=schema)
